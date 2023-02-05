ea0d687e2c8fa2b3b158096043230b0ba87c506e
NguyenXuanTruong02 <nguyenxuantruong231202@gmail.com>
2023-02-05 22:15:45 +0700

    fix id code reader

--- RFID.py
+++ RFID.py
@@ -1,15 +1,13 @@
 import PySimpleGUI as sg
 import pandas as pd
 import threading
-#from mfrc522 import SimpleMFRC522
+from mfrc522 import SimpleMFRC522
 
-#reader = SimpleMFRC522()
-
+reader = SimpleMFRC522()
 
 df = pd.read_excel(r'C:\Users\NGUYEN XUAN TRUONG\OneDrive\Tài liệu\Third year\Embedded Systems\test_GUI\RFID.xlsx')
 table_data = df.values.tolist()
 table_headings = df.columns.values.tolist()
-print(table_data)
 
 sg.theme("LightBlue")
 
@@ -21,7 +19,7 @@ layout = [
     sg.Text('Gender', size=(15,1)), sg.Combo(['Male', 'Female'], key = 'Gender',text_color = 'Black', size = (10,4))],
     [sg.Text('Plate number', size =(15,1)), sg.Input(key = 'Plate number', size = (50,4), do_not_clear = True)],
     [sg.Text('Phone number', size =(15,1)), sg.Input(key = 'Phone number', size = (50,4), do_not_clear = True)],
-    [sg.Button('Find ID'), sg.Text('ID: '), sg.Text(key='Output_ID'),],
+    [sg.Button('Input ID'), sg.Text('ID: '), sg.Input(key= 'Output_ID')],
     [sg.Button('Save'), sg.Button('Exit')],
     
 
@@ -38,59 +36,56 @@ layout = [
 
 window = sg.Window("RFID",layout)
 
-""" # NEW WINDOW TO FIND ID
+ # NEW WINDOW TO FIND ID
 def finding_ID_window():
+    reader = SimpleMFRC522()
+    ID_reader, text = reader.read()
+
     finding_ID_layout = [
-     [sg.Text("Searching ...", size=(20,1), justification = "Center")],
-     [sg.Input(key='ID', do_not_clear = True, size=(50,4))],
+     [sg.Text('ID Status'), sg.Text(key='ID_Status')],
+     [sg.Input(key='ID_code', do_not_clear = True, size=(50,4))],
      [sg.Button('Save')],
      [sg.Button('Exit')]                                             
                         ]
 
     finding_ID_window = sg.Window('ID Searching ...', finding_ID_layout, modal=True, size= (250,130))
 
+    finding_ID_window['ID_code'].update(ID_reader)
     while True:
         event, values = finding_ID_window.read()
         if event == sg.WIN_CLOSED or event == 'Exit':
             break
         elif event == 'Save':
-            try:
-                values['ID'] = int(values['ID'])
-            except ValueError:
-                window.write_event_value('Please enter a valid ID', None)
-                continue
-            else:
-                window.write_event_value('ID: ', values['ID'])
-                window['ID'].update(values['Output_ID'])
-                break
-        
-    
-    finding_ID_window.close()
-"""
-
-def search(ID_code): #FIND ID WITHOUT NEW WINDOW
-    while ID_code == None:
-        try:
-            values['ID'] = int(values['ID'])
-        except ValueError:
-            continue
-        else:
+            validate_id_code(values['ID_code'], finding_ID_window)
+            window['Output_ID'].update(values['ID_code'])
             break
-window.write_event_value('Done', None)
+        elif event == 'ID_Err':
+            finding_ID_window['ID_Status'].update(values[event])    
+    finding_ID_window.close()
 
+
+def validate_id_code(ID_code,window): #FIND ID WITHOUT NEW WINDOW
+    if ID_code == '':
+        window.write_event_value('ID_Err', 'Please enter a valid ID')
+    else:
+        try:
+            value = int(ID_code)
+            id_list = df.values[:,0].tolist()
+            if value in id_list:
+                window.write_event_value('ID_Err', 'ID already existed')
+        except:
+            window.write_event_value('ID_Err', 'Please enter a valid ID')  
+    
 
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
-    elif event == 'Find ID':
-        reader = SimpleMFRC522()
-        ID_code, text = reader.read()
-        window['Output_ID'].update('Searching ...')
-        threading.Thread(target= search, args=(ID_code)).start()
-    elif event == 'Done':
-        window['Done'].update(ID_code)
+    elif event == 'Input ID':
+        finding_ID_window()
+    
     elif event == 'Save':
         df = df.append(values, ignore_index = True)
         df.to_excel(r'C:\Users\NGUYEN XUAN TRUONG\OneDrive\Tài liệu\Third year\Embedded Systems\test_GUI\RFID.xlsx', index = False)
+
 window.close()
