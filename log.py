06bd44f24434c548cf555c35de7a4d7133284c16
tghung123456 <124576136+tghung123456@users.noreply.github.com>
2023-02-09 09:35:26 +0700

    Update log.py

--- log.py
+++ log.py
@@ -8,17 +8,21 @@ ws = wb["Sheet1"]
 
 
 def login(text):
-    ws.cell(row=ws.max_row + 1, column=1).value = text
-    ws.cell(row=ws.max_row, column=2).value = time.ctime(time.time())
-    wb.save("Log.xlsx")
-
+    i = 0
+    for row in ws.iter_rows(min_row=2, min_col=1, max_row=ws.max_row, max_col=1):
+        for cell in row:
+            if cell.value == text and ws.cell(row=cell.row, column=3).value is None:
+                i = 1
+            else:
+                i = 0
+    if i == 0:            
+        ws.cell(row=ws.max_row+1, column=1).value = text
+        ws.cell(row=ws.max_row, column=2).value = time.ctime(time.time())
+        wb.save("Log.xlsx")
 
 def logout(text):
     for row in ws.iter_rows(min_row=2, min_col=1, max_row=ws.max_row, max_col=1):
         for cell in row:
-            if cell.value == text:
+            if cell.value == text and ws.cell(row=cell.row, column=3).value is None:
                 ws.cell(row=cell.row, column=3).value = time.ctime(time.time())
                 wb.save("Log.xlsx")
-
-
-logout("29P188276")
