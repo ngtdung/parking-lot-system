8028e158d2c8bb0d23e6838e31852256048c85a1
tghung123456 <124576136+tghung123456@users.noreply.github.com>
2023-02-10 14:50:49 +0700

    Add files via upload

---
+++ logr.py
@@ -0,0 +1,36 @@
+from openpyxl import Workbook
+from openpyxl import load_workbook
+import time
+
+wb = load_workbook(filename="Log.xlsx")
+
+ws = wb["Sheet1"]
+
+def check(id):
+    i = 0
+    r = 0
+    plate = None
+    rows = list(ws.iter_rows(min_row=2, max_row=ws.max_row))
+    rows = reversed(rows)
+    for row in rows:
+        if row[0].value == id and row[2].value is None:
+            pos = row[3].coordinate
+            plate = image_loader.get(pos)
+            r = row
+            break
+    return plate, r
+
+def log(id, img_path, r):
+    i = 0
+    if r == 0:            
+        ws.cell(row=ws.max_row+1, column=1).value = id
+        ws.cell(row=ws.max_row, column=2).value = time.ctime(time.time())
+        ws.cell(row=ws.max_row, column=4).value = img_path
+    else:
+        ws.cell(row=r, column=3).value = time.ctime(time.time())
+        ws.cell(row=r, column=5).value = img_path
+        i = 1
+
+    wb.save("Log.xlsx")
+
+    return i
